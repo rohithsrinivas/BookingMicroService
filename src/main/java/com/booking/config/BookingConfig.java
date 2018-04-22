28664381396b2622aead016ffb011f4829b27785
rohithsrinivas <rohithsrinivas.94@gmail.com>
2018-04-22 11:47:18 +0530

    migranting from mysql to postgresql

--- src/main/java/com/booking/config/BookingConfig.java
+++ src/main/java/com/booking/config/BookingConfig.java
@@ -1,12 +1,17 @@
 package com.booking.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -35,6 +40,13 @@ public class BookingConfig {
 	public CacheManager cacheManager() {
 		return new GuavaCacheManager(BookingServiceConstants.CACHE_NAME_FOR_RESTAURANT_AND_BOOKINGS);
 	}
+	
+	@Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 	
 	
 }
