a7fa0d8c91942e098960bd2c6e72e53a7c253a49
rohithsrinivas <rohithsrinivas.94@gmail.com>
2018-04-22 16:10:56 +0530

    fix for media type issue

--- src/main/java/com/booking/controller/BookingController.java
+++ src/main/java/com/booking/controller/BookingController.java
@@ -22,60 +22,66 @@ import com.rest.util.PaymentUtils;
 
 @RestController
 public class BookingController {
-	
+
 	@Autowired
 	BookingService bookingService;
 
-	/*Using this endpoint we can get the list of all bookings that are currently available in the booking repository*/
-	@PostMapping(value="/createBooking",
-			consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Booking> createBooking(@RequestBody BookingInputDto bookingInputDto){
+	/*
+	 * Using this endpoint we can get the list of all bookings that are currently
+	 * available in the booking repository
+	 */
+	@PostMapping(value = "/createBooking", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Booking> createBooking(@RequestBody BookingInputDto bookingInputDto) {
 		this.executeUpdatesOnRestaurant(bookingInputDto);
 		Booking bookingEntity = this.prepareBookingDetails(bookingInputDto);
 		this.bookingService.placeBooking(bookingEntity);
 		return new ResponseEntity<Booking>(bookingEntity, HttpStatus.OK);
 	}
-	
-	/*Using this endpoint we can get the list of bookings made for a particular restaurant based on the restaurant ID*/
-	@GetMapping(value="/bookingsForRestaurant/{restaurantId}",
-			consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Booking>> getBookingsForRestaurant(
-			@PathVariable("restaurantId") Integer restaurantId){
-		return new ResponseEntity<List<Booking>>
-		(this.bookingService.getBookingsForRestaurant(restaurantId), HttpStatus.OK);
+
+	/*
+	 * Using this endpoint we can get the list of bookings made for a particular
+	 * restaurant based on the restaurant ID
+	 */
+	@GetMapping(value = "/bookingsForRestaurant/{restaurantId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Booking>> getBookingsForRestaurant(@PathVariable("restaurantId") Integer restaurantId) {
+		return new ResponseEntity<List<Booking>>(this.bookingService.getBookingsForRestaurant(restaurantId),
+				HttpStatus.OK);
 	}
-	
-	@GetMapping(value="/bookingsForUser/{bookedByUserId}",
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Booking>> getBookingsForUser(@PathVariable("bookedByUserId") String bookedByUserId){
-		return new ResponseEntity<List<Booking>>
-		(this.bookingService.getBookingsForUser(bookedByUserId),HttpStatus.OK);
+
+	@GetMapping(value = "/bookingsForUser/{bookedByUserId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Booking>> getBookingsForUser(@PathVariable("bookedByUserId") String bookedByUserId) {
+		return new ResponseEntity<List<Booking>>(this.bookingService.getBookingsForUser(bookedByUserId), HttpStatus.OK);
 	}
-	
-	/*Using this endpoint we can get the list of restaurants for a given name and required table count i.e by using
-	 * the Restaurant micro service, we fetch the filtered list of restaurants*/
-	@GetMapping(value="/restaurants/name/{restaurantName}/{numberOfTablesRequired}",produces=MediaType.APPLICATION_JSON_VALUE)
+
+	/*
+	 * Using this endpoint we can get the list of restaurants for a given name and
+	 * required table count i.e by using the Restaurant micro service, we fetch the
+	 * filtered list of restaurants
+	 */
+	@GetMapping(value = "/restaurants/name/{restaurantName}/{numberOfTablesRequired}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Restaurant>> getRestaurantsByName(
-			@PathVariable(name="restaurantName",required=true) String restaurantName,
-			@PathVariable(name="numberOfTablesRequired",required=true) Integer numberOfTablesRequired){
-			return new ResponseEntity<List<Restaurant>>
-			(this.bookingService.getRestaurantsByName(restaurantName, numberOfTablesRequired), HttpStatus.OK);
+			@PathVariable(name = "restaurantName", required = true) String restaurantName,
+			@PathVariable(name = "numberOfTablesRequired", required = true) Integer numberOfTablesRequired) {
+		return new ResponseEntity<List<Restaurant>>(
+				this.bookingService.getRestaurantsByName(restaurantName, numberOfTablesRequired), HttpStatus.OK);
 	}
-	
-	/*Using this endpoint we can get the list of restaurants for a given address and required table count i.e by using
-	 * the Restaurant micro service, we fetch the filtered list of restaurants*/
-	@GetMapping(value="/restaurants/address/{address}/{numberOfTablesRequired}",produces=MediaType.APPLICATION_JSON_VALUE)
+
+	/*
+	 * Using this endpoint we can get the list of restaurants for a given address
+	 * and required table count i.e by using the Restaurant micro service, we fetch
+	 * the filtered list of restaurants
+	 */
+	@GetMapping(value = "/restaurants/address/{address}/{numberOfTablesRequired}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Restaurant>> getRestaurantsByAddress(
-			@PathVariable(name="address",required=true) String address,
-			@PathVariable(name="numberOfTablesRequired",required=true) Integer numberOfTablesRequired) throws IOException{
-			return new ResponseEntity<List<Restaurant>>
-			(this.bookingService.getRestaurantsByAddress(address, numberOfTablesRequired), HttpStatus.OK);
+			@PathVariable(name = "address", required = true) String address,
+			@PathVariable(name = "numberOfTablesRequired", required = true) Integer numberOfTablesRequired)
+			throws IOException {
+		return new ResponseEntity<List<Restaurant>>(
+				this.bookingService.getRestaurantsByAddress(address, numberOfTablesRequired), HttpStatus.OK);
 	}
-	
-	
-	
+
 	private Booking prepareBookingDetails(BookingInputDto bookingInputDto) {
-		Booking booking=new Booking();
+		Booking booking = new Booking();
 		booking.setNumberOfTablesBooked(bookingInputDto.getNoOfTablesBooked());
 		booking.setOrderedItems(bookingInputDto.getOrderedItems());
 		booking.setTableCategory(bookingInputDto.getTableCategory());
@@ -83,21 +89,20 @@ public class BookingController {
 		booking.setRestaurant(bookingInputDto.getRestaurant());
 		booking.setBookingDate(new Date().toString());
 		booking.setBookedByUserId(bookingInputDto.getBookedByUserId());
-		if(bookingInputDto.getOrderedItems()!=null && bookingInputDto.getOrderedItems().size()>0)
-		booking.setBillingAmount(PaymentUtils.calculateBill(bookingInputDto.getOrderedItems()));
+		if (bookingInputDto.getOrderedItems() != null && bookingInputDto.getOrderedItems().size() > 0)
+			booking.setBillingAmount(PaymentUtils.calculateBill(bookingInputDto.getOrderedItems()));
 		else
 			booking.setBillingAmount(0D);
 		return booking;
 	}
-	
-	
 
-	/*this functionality is to update the number of tables in the restaurant entity when a booking is placed for a particular
-	 * number of tables i.e to reduce those many number of available tables in restaurant entity*/
+	/*
+	 * this functionality is to update the number of tables in the restaurant entity
+	 * when a booking is placed for a particular number of tables i.e to reduce
+	 * those many number of available tables in restaurant entity
+	 */
 	private void executeUpdatesOnRestaurant(BookingInputDto bookingInputDto) {
 		this.bookingService.updateRestaurantEntity(bookingInputDto);
 	}
-	
-	
 
 }
